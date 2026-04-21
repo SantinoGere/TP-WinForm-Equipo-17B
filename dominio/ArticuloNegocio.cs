0a40d5acc7bc0cbfcdeedf040e1cb41472940666
Santino Gerevini <santinogerevini8@gmail.com>
2026-04-21 20:17:12 -0300

    Agregar Clase ArticuloNegocio
    Implement ArticuloNegocio class with listar method to retrieve articles.

---
+++ dominio/ArticuloNegocio.cs
@@ -0,0 +1,25 @@
+public class ArticuloNegocio
+{
+    public List<Articulo> listar()
+    {
+        List<Articulo> lista = new List<Articulo>();
+        AccesoDatos datos = new AccesoDatos();
+
+        datos.setearConsulta("SELECT A.Id, Codigo, Nombre, A.Descripcion, Precio, M.Descripcion Marca, C.Descripcion Categoria FROM ARTICULOS A, MARCAS M, CATEGORIAS C WHERE A.IdMarca = M.Id AND A.IdCategoria = C.Id");
+
+        datos.ejecutarLectura();
+
+        while (datos.Lector.Read())
+        {
+            Articulo aux = new Articulo();
+            aux.Id = (int)datos.Lector["Id"];
+            aux.Nombre = (string)datos.Lector["Nombre"];
+            aux.Marca = new Marca();
+            aux.Marca.Descripcion = (string)datos.Lector["Marca"];
+
+            lista.Add(aux);
+        }
+
+        return lista;
+    }
+}
