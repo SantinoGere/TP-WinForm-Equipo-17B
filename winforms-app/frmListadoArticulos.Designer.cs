52c3282d7422e209ddbd141cdda7041792553ea6
Santino Gerevini <santinogerevini8@gmail.com>
2026-04-24 20:21:51 -0300

    Add labels and picture box to frmListadoArticulos

--- winforms-app/frmListadoArticulos.Designer.cs
+++ winforms-app/frmListadoArticulos.Designer.cs
@@ -4,6 +4,10 @@ namespace winforms_app
     {
         private System.ComponentModel.IContainer components = null;
         private System.Windows.Forms.DataGridView dgvArticulos;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.PictureBox pbArticulo;
+
 
         protected override void Dispose(bool disposing)
         {
@@ -19,7 +23,13 @@ namespace winforms_app
         private void InitializeComponent()
         {
             this.dgvArticulos = new System.Windows.Forms.DataGridView();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.pbArticulo = new System.Windows.Forms.PictureBox();
+
             ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prArticulo)).BeginInit();
+
             this.SuspendLayout();
             // 
             // dgvArticulos
@@ -31,19 +41,50 @@ namespace winforms_app
             this.dgvArticulos.Size = new System.Drawing.Size(1773, 959);
             this.dgvArticulos.TabIndex = 0;
             this.dgvArticulos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvArticulos_CellContentClick);
-            // 
+            
+            // lblNombre
+            this.lblNombre.AutoSize = True;
+            this.lblNombre.Location = new System.Drawin.Point(960, 50);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(47, 13);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "Nombre";
+
+             // lblDescripcion
+            this.lblDescripcion.AutoSize = True;
+            this.lblDescripcion.Location = new System.Drawin.Point(960, 80);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(66, 13);
+            this.lblDescripcion.TabIndex = 2;
+            this.lblDescripcion.Text = "Descripcion";
+
+             // pbArticulo
+            this.pbArticulo.Location = new System.Drawin.Point(960, 110);
+            this.pbArticulo.Name = "pbArticulo";
+            this.pbArticulo.Size = new System.Drawing.Size(250, 200);
+            this.pbArticulo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbArticulo.TabIndex = 3;
+            this.pbArticulo.TabStop = false;
+            
+            
             // frmListadoArticulos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1849, 1027);
             this.Controls.Add(this.dgvArticulos);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblDescripcion);
+            this.Controls.Add(this.pbArticulo); 
             this.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
             this.Name = "frmListadoArticulos";
             this.Text = "Listado de Artículos";
             this.Load += new System.EventHandler(this.frmListadoArticulos_Load);
+            
             ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbArticulo)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
